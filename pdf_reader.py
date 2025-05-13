d688b2270eea00e204345315e0ef667eb51910a6
rithikashrii <rithikashris1@gmail.com>
2025-05-13 19:14:31 +0530

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,7 +1,7 @@
 import PyPDF2
 
 def extract_text_from_pdf(pdf_file):
-    reader = pypdf2.PdfReader(pdf_file)
+    reader = PyPDF2.PdfReader(pdf_file)
     text = ""
     for page in reader.pages:
         text += page.extract_text() or ""
